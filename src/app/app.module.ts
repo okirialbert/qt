397e14d66acffa055b9ec71cd3807987f9087fbc
devlp121 <okirialbert@gmail.com>
2019-06-12 06:24:35 +0300

    Second deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrintingComponent } from "./printing/printing.component";
+import { ProcedureComponent } from "./procedure/procedure.component";
+import { DurationComponent } from "./duration/duration.component";
+import { FeeComponent } from "./fee/fee.component";
+import { MarketingComponent } from "./marketing/marketing.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProcedureComponent,
+    DurationComponent,
+    FeeComponent,
+    MarketingComponent,
+    PrintingComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
